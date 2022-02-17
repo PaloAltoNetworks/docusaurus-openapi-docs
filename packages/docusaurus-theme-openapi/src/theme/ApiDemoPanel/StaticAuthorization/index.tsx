2582a1c2d9cc816c5d4ded151df5f5101282e8b6
Bryan <blindaa121@gmail.com>
2022-02-16 16:39:58 -0800

    Create StaticAuthorization component

---
+++ packages/docusaurus-theme-openapi/src/theme/ApiDemoPanel/StaticAuthorization/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useTypedSelector } from "../hooks";
+
+function StaticAuthorization() {
+  const options = useTypedSelector((state) => state.auth.options);
+  const selected = useTypedSelector((state) => state.auth.selected);
+
+  if (selected === undefined) return null;
+
+  const selectedAuth = options[selected];
+
+  return (
+    <div style={{ marginBottom: "var(--ifm-table-cell-padding)" }}>
+      {selectedAuth.map((auth) => {
+        if (auth.type === "apiKey") {
+          return (
+            <React.Fragment key={selected + "-apiKey"}>
+              <b>{auth.type} Authorization</b>
+              <pre
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  background: "var(--openapi-card-background-color)",
+                  borderRadius: "var(--openapi-card-border-radius)",
+                }}
+              >
+                <span>name: {auth.name}</span>
+                <span>in: {auth.in}</span>
+                <span>type: {auth.type}</span>
+              </pre>
+            </React.Fragment>
+          );
+        }
+
+        return null;
+      })}
+    </div>
+  );
+}
+
+export default StaticAuthorization;
