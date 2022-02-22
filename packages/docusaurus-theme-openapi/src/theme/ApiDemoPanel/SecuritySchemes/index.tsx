dd644ab0b54beedab01f2e55d16876b6549a22ce
Bryan <blindaa121@gmail.com>
2022-02-22 13:51:50 -0800

    Fix linting issues

--- packages/docusaurus-theme-openapi/src/theme/ApiDemoPanel/SecuritySchemes/index.tsx
+++ packages/docusaurus-theme-openapi/src/theme/ApiDemoPanel/SecuritySchemes/index.tsx
@@ -1,4 +1,12 @@
+/* ============================================================================
+ * Copyright (c) Cloud Annotations
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
 import React from "react";
+
 import { useTypedSelector } from "../hooks";
 
 function SecuritySchemes() {
