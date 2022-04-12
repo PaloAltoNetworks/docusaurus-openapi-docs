868c83f5214ffd48e8fdfe0cc592be61ded00a6d
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-04-12 17:30:30 -0400

    Add @docusaurus/plugin-content-docs-types declaration

--- packages/docusaurus-theme-openapi/src/theme-openapi.d.ts
+++ packages/docusaurus-theme-openapi/src/theme-openapi.d.ts
@@ -7,6 +7,42 @@
 
 /// <reference types="@paloaltonetworks/docusaurus-plugin-openapi" />
 
+declare module "@docusaurus/plugin-content-docs-types" {
+  // Makes all properties visible when hovering over the type
+  type Expand<T extends Record<string, unknown>> = { [P in keyof T]: T[P] };
+
+  export type SidebarItemBase = {
+    className?: string;
+    customProps?: Record<string, unknown>;
+  };
+
+  export type SidebarItemLink = SidebarItemBase & {
+    type: "link";
+    href: string;
+    label: string;
+    docId: string;
+  };
+
+  type SidebarItemCategoryBase = SidebarItemBase & {
+    type: "category";
+    label: string;
+    collapsed: boolean;
+    collapsible: boolean;
+  };
+
+  export type PropSidebarItemCategory = Expand<
+    SidebarItemCategoryBase & {
+      items: PropSidebarItem[];
+    }
+  >;
+
+  export type PropSidebarItem = SidebarItemLink | PropSidebarItemCategory;
+  export type PropSidebar = PropSidebarItem[];
+  export type PropSidebars = {
+    [sidebarId: string]: PropSidebar;
+  };
+}
+
 declare module "docusaurus-theme-openapi" {
   export type ThemeConfig = Partial<import("./types").ThemeConfig>;
 }
