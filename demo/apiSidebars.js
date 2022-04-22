8f08839d5311ce23768e3e74a364f5c8c2acd458
Bryan <blindaa121@gmail.com>
2022-04-21 20:01:27 -0700

    Add .js file extention to petstore/sidebar

--- demo/apiSidebars.js
+++ demo/apiSidebars.js
@@ -24,7 +24,7 @@ const sidebars = {
           "This is a sample server Petstore server. You can find out more about Swagger at http://swagger.io or on irc.freenode.net, #swagger. For this sample, you can use the api key special-key to test the authorization filters.",
         slug: "/category/petstore-api",
       },
-      items: require("./api/petstore/sidebar").sidebar,
+      items: require("./api/petstore/sidebar.js").sidebar,
     },
     {
       type: "category",
