9b168b9e9242b05109be4a76beb37bc2114332c9
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-04-27 16:17:08 -0400

    Update header and eslint header rule

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env ts-node
 /* ============================================================================
- * Copyright (c) Cloud Annotations
+ * Copyright (c) Palo Alto Networks
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
