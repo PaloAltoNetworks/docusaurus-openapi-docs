a505672a7157115a23f904bc237d373fc3ddae6c
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-04-28 14:46:13 -0400

    Cleanup types and fix paginator #66

--- packages/docusaurus-theme-openapi/src/theme/ApiItem/index.tsx
+++ packages/docusaurus-theme-openapi/src/theme/ApiItem/index.tsx
@@ -8,9 +8,10 @@
 import React from "react";
 
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import type { ApiItem as ApiItemType } from "@paloaltonetworks/docusaurus-plugin-openapi/lib/types";
 import DocItem from "@theme-original/DocItem";
-// @ts-ignore
-import type { Props } from "@theme/ApiItem";
+import type { Props, FrontMatter } from "@theme/DocItem";
+import DocItemFooter from "@theme/DocItemFooter";
 import DocPaginator from "@theme/DocPaginator";
 import clsx from "clsx";
 
@@ -20,30 +21,39 @@ let ApiDemoPanel = (_: { item: any }) => <div style={{ marginTop: "3.5em" }} />;
 if (ExecutionEnvironment.canUseDOM) {
   ApiDemoPanel = require("@theme/ApiDemoPanel").default;
 }
+interface ApiFrontMatter extends FrontMatter {
+  readonly api?: ApiItemType;
+}
 
-export default function ApiItem(props: typeof Props): JSX.Element {
-  let api = props.content.frontMatter.api ?? false;
-  if (!api) {
-    return (
-      <>
-        <DocItem {...props} />
-      </>
-    );
-  } else {
-    let next = props.content.metadata.next;
-    let previous = props.content.metadata.previous;
-    props.content.metadata.next = null;
-    props.content.metadata.previous = null;
-    props.content.metadata.toc = null;
+export default function ApiItem(props: Props): JSX.Element {
+  // Make deep copy of props so we can show/hide DocItem features
+  // as needed when used to wrap api docs.
+  const apiDocItemProps = Object.assign({}, props);
+  const { content: DocItemContent } = apiDocItemProps;
+  const { metadata, frontMatter } = DocItemContent;
+  const { api } = frontMatter as ApiFrontMatter;
+  const { previous, next } = metadata;
+
+  if (api) {
+    (metadata.next as any) = null;
+    (metadata.previous as any) = null;
+    (metadata.lastUpdatedAt as any) = null;
+    (metadata.lastUpdatedBy as any) = null;
+    (frontMatter.hide_table_of_contents as any) = true;
     return (
       <div className={styles.apiItemContainer}>
         <div className={clsx("theme-api-markdown", "markdown")}>
           <div className="row">
             <div className="col col--7">
-              <DocItem {...props} />
+              <DocItem {...apiDocItemProps} />
             </div>
             <div className="col col--5">
               <ApiDemoPanel item={api} />
+            </div>
+          </div>
+          <div className="row">
+            <div className={clsx("col col--7")}>
+              <DocItemFooter {...props} />
             </div>
           </div>
           <div className="row">
@@ -53,6 +63,12 @@ export default function ApiItem(props: typeof Props): JSX.Element {
           </div>
         </div>
       </div>
+    );
+  } else {
+    return (
+      <>
+        <DocItem {...props} />
+      </>
     );
   }
 }
