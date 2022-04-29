b8a6acf7b8f116b5c72a9f984580392a668d33dc
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-04-29 10:13:11 -0400

    Refactor ApiItem based on DocItem

--- packages/docusaurus-theme-openapi/src/theme/ApiItem/index.tsx
+++ packages/docusaurus-theme-openapi/src/theme/ApiItem/index.tsx
@@ -8,13 +8,24 @@
 import React from "react";
 
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import {
+  PageMetadata,
+  HtmlClassNameProvider,
+  ThemeClassNames,
+  useWindowSize,
+} from "@docusaurus/theme-common";
 import type { ApiItem as ApiItemType } from "@paloaltonetworks/docusaurus-plugin-openapi/lib/types";
-import DocItem from "@theme-original/DocItem";
+import DocBreadcrumbs from "@theme/DocBreadcrumbs";
 import type { Props, FrontMatter } from "@theme/DocItem";
 import DocItemFooter from "@theme/DocItemFooter";
 import DocPaginator from "@theme/DocPaginator";
+import DocVersionBadge from "@theme/DocVersionBadge";
+import DocVersionBanner from "@theme/DocVersionBanner";
+import Heading from "@theme/Heading";
+import MDXContent from "@theme/MDXContent";
+import TOC from "@theme/TOC";
+import TOCCollapsible from "@theme/TOCCollapsible";
 import clsx from "clsx";
-// import cloneDeep from "lodash/cloneDeep";
 
 import styles from "./styles.module.css";
 
@@ -22,56 +33,129 @@ let ApiDemoPanel = (_: { item: any }) => <div style={{ marginTop: "3.5em" }} />;
 if (ExecutionEnvironment.canUseDOM) {
   ApiDemoPanel = require("@theme/ApiDemoPanel").default;
 }
+
 interface ApiFrontMatter extends FrontMatter {
   readonly api?: ApiItemType;
 }
 
-export default function ApiItem(props: Props): JSX.Element {
-  const { previous, next } = props.content.metadata;
+function DocItemMetadata(props: Props): JSX.Element {
+  const { content: DocContent } = props;
+  const { metadata, frontMatter, assets } = DocContent;
+  const { keywords } = frontMatter;
+  const { description, title } = metadata;
+  const image = assets.image ?? frontMatter.image;
 
-  // Make deep copy of props so we can show/hide DocItem features
-  // as needed when used to wrap api docs.
-  const apiDocItemProps = { ...props };
-  const { content: ApiItemContent } = apiDocItemProps;
-  const { metadata, frontMatter } = ApiItemContent;
+  return <PageMetadata {...{ title, description, keywords, image }} />;
+}
+
+function DocItemContent(props: Props): JSX.Element {
+  const { content: DocContent } = props;
+  const { metadata, frontMatter } = DocContent;
+  const {
+    hide_title: hideTitle,
+    hide_table_of_contents: hideTableOfContents,
+    toc_min_heading_level: tocMinHeadingLevel,
+    toc_max_heading_level: tocMaxHeadingLevel,
+  } = frontMatter;
+  const { title } = metadata;
+
   const { api } = frontMatter as ApiFrontMatter;
 
-  if (api) {
-    (metadata.next as any) = null;
-    (metadata.previous as any) = null;
-    // (metadata.lastUpdatedAt as any) = null;
-    // (metadata.lastUpdatedBy as any) = null;
-    (frontMatter.hide_table_of_contents as any) = true;
-    return (
-      <div className={styles.apiItemContainer}>
-        <div className={clsx("theme-api-markdown", "markdown")}>
-          <div className="row">
-            <div className="col col--7">
-              <DocItem {...apiDocItemProps} />
-            </div>
-            <div className="col col--5">
-              <ApiDemoPanel item={api} />
-            </div>
-          </div>
+  // We only add a title if:
+  // - user doesn't ask to hide it with front matter
+  // - the markdown content does not already contain a top-level h1 heading
+  const shouldAddTitle =
+    !hideTitle && typeof DocContent.contentTitle === "undefined";
 
-          <div className="row">
-            <div className={clsx("col col--7")}>
+  const windowSize = useWindowSize();
+
+  const canRenderTOC =
+    !hideTableOfContents && DocContent.toc && DocContent.toc.length > 0;
+
+  const renderTocDesktop =
+    canRenderTOC && (windowSize === "desktop" || windowSize === "ssr");
+
+  return (
+    <div className="row">
+      <div className={clsx("col", !hideTableOfContents && styles.docItemCol)}>
+        <DocVersionBanner />
+        <div className={styles.docItemContainer}>
+          <article>
+            <DocBreadcrumbs />
+            <DocVersionBadge />
+
+            {canRenderTOC && (
+              <TOCCollapsible
+                toc={DocContent.toc}
+                minHeadingLevel={tocMinHeadingLevel}
+                maxHeadingLevel={tocMaxHeadingLevel}
+                className={clsx(
+                  ThemeClassNames.docs.docTocMobile,
+                  styles.tocMobile
+                )}
+              />
+            )}
+
+            <div
+              className={clsx(
+                ThemeClassNames.docs.docMarkdown,
+                "theme-api-markdown",
+                "markdown"
+              )}
+            >
+              {/*
+                Title can be declared inside md content or declared through
+                front matter and added manually. To make both cases consistent,
+                the added title is added under the same div.markdown block
+                See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
+                */}
+              {shouldAddTitle && (
+                <header>
+                  <Heading as="h1">{title}</Heading>
+                </header>
+              )}
+              <MDXContent>
+                <div className="row">
+                  <div className={clsx("col", api ? "col--7" : "col--9")}>
+                    <DocContent />
+                  </div>
+                  {api && (
+                    <div className={clsx("col", api && "col--5")}>
+                      <ApiDemoPanel item={api} />
+                    </div>
+                  )}
+                </div>
+              </MDXContent>
+            </div>
+            <div className={clsx("col", api ? "col--7" : "col--9")}>
               <DocItemFooter {...props} />
             </div>
-          </div>
-          <div className="row">
-            <div className={clsx("col col--7")}>
-              <DocPaginator previous={previous} next={next} />
-            </div>
+          </article>
+          <div className={clsx("col", api ? "col--7" : "col--9")}>
+            <DocPaginator previous={metadata.previous} next={metadata.next} />
           </div>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <>
-        <DocItem {...props} />
-      </>
-    );
-  }
+      {renderTocDesktop && (
+        <div className="col col--3">
+          <TOC
+            toc={DocContent.toc}
+            minHeadingLevel={tocMinHeadingLevel}
+            maxHeadingLevel={tocMaxHeadingLevel}
+            className={ThemeClassNames.docs.docTocDesktop}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function DocItem(props: Props): JSX.Element {
+  const docHtmlClassName = `docs-doc-id-${props.content.metadata.unversionedId}`;
+  return (
+    <HtmlClassNameProvider className={docHtmlClassName}>
+      <DocItemMetadata {...props} />
+      <DocItemContent {...props} />
+    </HtmlClassNameProvider>
+  );
 }
