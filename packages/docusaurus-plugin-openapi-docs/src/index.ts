40ec23b977c595734eac51dbe4d03cdac108be77
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-05-23 09:46:37 -0500

    Fix categoryLinkSource reference

--- packages/docusaurus-plugin-openapi-docs/src/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/index.ts
@@ -155,7 +155,7 @@ import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
         if (item.type === "info") {
           if (!fs.existsSync(`${outputDir}/${item.id}.info.mdx`)) {
             try {
-              sidebarOptions?.useInfoAsCategoryLink
+              sidebarOptions?.categoryLinkSource === "info" // Only use utils template if set to "info"
                 ? fs.writeFileSync(
                     `${outputDir}/${item.id}.info.mdx`,
                     utils,
