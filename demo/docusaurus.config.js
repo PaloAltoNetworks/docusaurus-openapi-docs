07f46f1b4081ecce98da57c90fd00aaf74963a7c
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-05-24 15:05:04 -0500

    Update logo and github urls

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -53,8 +53,8 @@ const config = {
       navbar: {
         title: "OpenAPI Docs",
         logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
+          alt: "Keytar",
+          src: "img/docusaurus-openapi-docs-logo.svg",
         },
         items: [
           {
@@ -70,7 +70,7 @@ const config = {
             to: "/docs/category/petstore-api",
           },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/PaloAltoNetworks/docusaurus-openapi-docs",
             label: "GitHub",
             position: "right",
           },
@@ -114,12 +114,12 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/PaloAltoNetworks/docusaurus-openapi-docs",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Palo Alto Networks, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
