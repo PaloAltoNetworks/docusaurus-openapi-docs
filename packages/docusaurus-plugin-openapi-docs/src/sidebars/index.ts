0f77384d3608a6f1562d848071cf54fd5eb166fb
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-06-07 12:59:58 -0500

    Remove unused import

--- packages/docusaurus-plugin-openapi-docs/src/sidebars/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/sidebars/index.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-import { GlobalPluginData } from "@docusaurus/plugin-content-docs/client";
 import {
   ProcessedSidebar,
   SidebarItemCategory,
