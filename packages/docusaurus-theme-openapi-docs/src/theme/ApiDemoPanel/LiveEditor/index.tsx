caa49b8c8ff1062a84966ee560f07edf9a22f7f9
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-06-14 09:38:03 -0500

    Add new LiveEditor

---
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/LiveEditor/index.tsx
@@ -0,0 +1,59 @@
+/* ============================================================================
+ * Copyright (c) Palo Alto Networks
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
+import React from "react";
+
+import { usePrismTheme } from "@docusaurus/theme-common";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import { LiveProvider, LiveEditor, withLive } from "react-live";
+
+import { setStringRawBody } from "../Body/slice";
+import styles from "./styles.module.css";
+
+function Live({ onEdit }: any) {
+  const isBrowser = useIsBrowser();
+  return (
+    <>
+      <LiveEditor
+        key={String(isBrowser)}
+        className={styles.playgroundEditor}
+        onChange={onEdit}
+      />
+    </>
+  );
+}
+
+const LiveComponent = withLive(Live);
+
+function App({
+  children,
+  transformCode,
+  value,
+  language,
+  action,
+  ...props
+}: any): JSX.Element {
+  const prismTheme = usePrismTheme();
+  const [code, setCode] = React.useState(children);
+  action(setStringRawBody(code));
+  return (
+    <div className={styles.playgroundContainer}>
+      <LiveProvider
+        code={children.replace(/\n$/, "")}
+        transformCode={transformCode ?? ((code) => `${code};`)}
+        theme={prismTheme}
+        language={language}
+        {...props}
+      >
+        <LiveComponent onEdit={setCode} />
+      </LiveProvider>
+    </div>
+  );
+}
+
+const LiveApp = withLive(App);
+export default LiveApp;
