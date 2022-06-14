50dae92fcb1ef281b90699e0496e14a3abeec647
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-06-14 09:20:16 -0500

    Make sidebar hideable

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -40,6 +40,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
         title: "OpenAPI Docs",
         logo: {
