65353dd516b37f14fa757a3c26dfbeb23d918f3b
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-07-11 09:30:32 -0500

    Add swagger property to OpenApiObject type

--- packages/docusaurus-plugin-openapi-docs/src/openapi/types.ts
+++ packages/docusaurus-plugin-openapi-docs/src/openapi/types.ts
@@ -20,6 +20,7 @@ export interface OpenApiObject {
   security?: SecurityRequirementObject[];
   tags?: TagObject[];
   externalDocs?: ExternalDocumentationObject;
+  swagger?: string;
 }
 
 export interface OpenApiObjectWithRef {
