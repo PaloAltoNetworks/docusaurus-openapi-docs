300095cb663b4b220ac20732a4b482faf36bde91
Bryan <blindaa121@gmail.com>
2022-08-01 16:44:11 -0700

    Remove unncessary code from ResponseSamples

--- packages/docusaurus-theme-openapi-docs/src/theme/ResponseSamples/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/ResponseSamples/index.js
@@ -8,68 +8,18 @@
 import React from "react";
 
 import CodeBlock from "@theme/CodeBlock";
-import SchemaTabs from "@theme/SchemaTabs";
-import TabItem from "@theme/TabItem";
-import clsx from "clsx";
 
 import styles from "./styles.module.css";
 
-function ResponseSamples({
-  responseExamples,
-  responseContentKey: contentType,
-}) {
-  const hasMultipleExamples = Object.keys(responseExamples).length > 1;
-
-  const renderResponseSamples = () => {
-    if (hasMultipleExamples) {
-      return (
-        <SchemaTabs>
-          {Object.entries(responseExamples).map(
-            ([exampleKey, exampleValue]) => (
-              <TabItem label={exampleKey} value={exampleKey}>
-                <pre
-                  className={clsx(styles.contentType, "codeBlock")}
-                >{`Content Type: ${contentType}`}</pre>
-                <CodeBlock
-                  language="javascript"
-                  className={styles.responseSamplesCodeBlock}
-                >
-                  {JSON.stringify(exampleValue.value, null, 2)}
-                </CodeBlock>
-              </TabItem>
-            )
-          )}
-        </SchemaTabs>
-      );
-    } else {
-      const responseExampleKey = Object.keys(responseExamples)[0];
-      const responseSampleContent = JSON.stringify(
-        responseExamples[responseExampleKey].value,
-        null,
-        2
-      );
-
-      return (
-        <>
-          <pre
-            className={clsx(styles.contentType, "codeBlock")}
-          >{`Content Type: ${contentType}`}</pre>
-          <CodeBlock
-            language="javascript"
-            className={styles.responseSamplesCodeBlock}
-          >
-            {responseSampleContent}
-          </CodeBlock>
-        </>
-      );
-    }
-  };
-
+function ResponseSamples({ responseExample }) {
   return (
     <div className={styles.responseSamplesContainer}>
-      <strong className={styles.responseSamplesHeader}>Response Samples</strong>
-      <hr />
-      {renderResponseSamples()}
+      <CodeBlock
+        language="javascript"
+        className={styles.responseSamplesCodeBlock}
+      >
+        {responseExample}
+      </CodeBlock>
     </div>
   );
 }
