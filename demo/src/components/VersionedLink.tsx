a7edd1d25af116f63d9d379ed3f2d462a6a5629b
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-08-03 16:50:27 -0400

    Apply Hasura styles

---
+++ demo/src/components/VersionedLink.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+
+const VersionedLink = ({ to, ...props }) => (
+  <Link to={`/latest${to}`} {...props} />
+);
+
+export default VersionedLink;
