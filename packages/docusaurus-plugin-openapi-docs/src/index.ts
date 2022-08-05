abc1d6df00995c3d59a51cfc2ea61dd776554ac5
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-08-04 21:33:07 -0400

    Cleanup API frontmatter

--- packages/docusaurus-plugin-openapi-docs/src/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/index.ts
@@ -148,9 +148,6 @@ hide_title: true
 {{#api}}
 hide_table_of_contents: true
 {{/api}}
-{{#json}}
-api: {{{json}}}
-{{/json}}
 {{#api.method}}
 sidebar_class_name: "{{{api.method}}} api-method"
 {{/api.method}}
