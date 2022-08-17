edac51d636655d8b69293a732eab904954dca887
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-08-17 13:20:48 -0400

    [Experimental] Introduce PWA support (#216)
    * PWA Test
    
    * Add PWA manifest
    
    * Change icon to 144x144
    
    * Update logo path
    
    * Adjust img sizes
    
    * Update url
    
    * Add splash image and update theme color
    
    * Expand pwa images for different platforms
    
    * Add maskable icon
    
    * Add apple touch icon
    
    * Add apple-touch-icon link to head
    
    * Add closing tag to head link

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -190,6 +190,34 @@ const config = {
         },
       },
     ],
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/docusaurus-openapi-docs-logo.svg",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json", // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+        ],
+      },
+    ],
   ],
 
   themes: ["docusaurus-theme-openapi-docs"],
