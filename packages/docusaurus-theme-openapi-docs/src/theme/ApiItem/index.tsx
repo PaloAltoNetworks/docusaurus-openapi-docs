761007f70df6c14ebddc1b61fa31b940cb96c10a
Bryan <blindaa121@gmail.com>
2022-10-05 10:28:33 -0700

    Fix ApiItem prod build errors

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/index.tsx
@@ -29,97 +29,104 @@ const { DocProvider } = require("@docusaurus/theme-common/internal");
 let ApiDemoPanel = (_: { item: any; infoPath: any }) => (
   <div style={{ marginTop: "3.5em" }} />
 );
-if (ExecutionEnvironment.canUseDOM) {
-  ApiDemoPanel = require("@theme/ApiDemoPanel").default;
-}
+
+let DocItem = (props: Props) => {
+  return <div style={{ marginTop: "3.5em" }} />;
+};
 
 interface ApiFrontMatter extends DocFrontMatter {
   readonly api?: ApiItemType;
 }
 
-export default function DocItem(props: Props): JSX.Element {
-  const docHtmlClassName = `docs-doc-id-${props.content.metadata.unversionedId}`;
-  const MDXComponent = props.content;
-  const { frontMatter } = MDXComponent;
-  const { info_path: infoPath } = frontMatter as DocFrontMatter;
-  const { api } = frontMatter as ApiFrontMatter;
-  const { siteConfig } = useDocusaurusContext();
-  const themeConfig = siteConfig.themeConfig as ThemeConfig;
-  const options = themeConfig.api;
+if (ExecutionEnvironment.canUseDOM) {
+  ApiDemoPanel = require("@theme/ApiDemoPanel").default;
 
-  const acceptArray = Array.from(
-    new Set(
-      Object.values(api?.responses ?? {})
-        .map((response: any) => Object.keys(response.content ?? {}))
-        .flat()
-    )
-  );
+  DocItem = function DocItem(props: Props): JSX.Element {
+    const docHtmlClassName = `docs-doc-id-${props.content.metadata.unversionedId}`;
+    const MDXComponent = props.content;
+    const { frontMatter } = MDXComponent;
+    const { info_path: infoPath } = frontMatter as DocFrontMatter;
+    const { api } = frontMatter as ApiFrontMatter;
+    const { siteConfig } = useDocusaurusContext();
+    const themeConfig = siteConfig.themeConfig as ThemeConfig;
+    const options = themeConfig.api;
 
-  const content = api?.requestBody?.content ?? {};
-  const contentTypeArray = Object.keys(content);
-  const servers = api?.servers ?? [];
+    const acceptArray = Array.from(
+      new Set(
+        Object.values(api?.responses ?? {})
+          .map((response: any) => Object.keys(response.content ?? {}))
+          .flat()
+      )
+    );
 
-  const params = {
-    path: [] as ParameterObject[],
-    query: [] as ParameterObject[],
-    header: [] as ParameterObject[],
-    cookie: [] as ParameterObject[],
-  };
+    const content = api?.requestBody?.content ?? {};
+    const contentTypeArray = Object.keys(content);
+    const servers = api?.servers ?? [];
 
-  api?.parameters?.forEach(
-    (param: { in: "path" | "query" | "header" | "cookie" }) => {
-      const paramType = param.in;
-      const paramsArray: ParameterObject[] = params[paramType];
-      paramsArray.push(param as ParameterObject);
-    }
-  );
+    const params = {
+      path: [] as ParameterObject[],
+      query: [] as ParameterObject[],
+      header: [] as ParameterObject[],
+      cookie: [] as ParameterObject[],
+    };
 
-  const auth = createAuth({
-    security: api?.security,
-    securitySchemes: api?.securitySchemes,
-    options,
-  });
+    api?.parameters?.forEach(
+      (param: { in: "path" | "query" | "header" | "cookie" }) => {
+        const paramType = param.in;
+        const paramsArray: ParameterObject[] = params[paramType];
+        paramsArray.push(param as ParameterObject);
+      }
+    );
 
-  const persistanceMiddleware = createPersistanceMiddleware(options);
+    const auth = createAuth({
+      security: api?.security,
+      securitySchemes: api?.securitySchemes,
+      options,
+    });
 
-  const store2 = createStoreWithState(
-    {
-      accept: { value: acceptArray[0], options: acceptArray },
-      contentType: { value: contentTypeArray[0], options: contentTypeArray },
-      server: { value: servers[0], options: servers },
-      response: { value: undefined },
-      body: { type: "empty" },
-      params,
-      auth,
-    },
-    [persistanceMiddleware]
-  );
+    const persistanceMiddleware = createPersistanceMiddleware(options);
 
-  const DocContent = () => {
-    return (
-      <div className="row">
-        <div className={clsx("col", api ? "col--7" : "col--12")}>
-          <MDXComponent />
-        </div>
-        {api && (
-          <div className="col col--5">
-            <ApiDemoPanel item={api} infoPath={infoPath} />
+    const store2 = createStoreWithState(
+      {
+        accept: { value: acceptArray[0], options: acceptArray },
+        contentType: { value: contentTypeArray[0], options: contentTypeArray },
+        server: { value: servers[0], options: servers },
+        response: { value: undefined },
+        body: { type: "empty" },
+        params,
+        auth,
+      },
+      [persistanceMiddleware]
+    );
+
+    const DocContent = () => {
+      return (
+        <div className="row">
+          <div className={clsx("col", api ? "col--7" : "col--12")}>
+            <MDXComponent />
           </div>
-        )}
-      </div>
+          {api && (
+            <div className="col col--5">
+              <ApiDemoPanel item={api} infoPath={infoPath} />
+            </div>
+          )}
+        </div>
+      );
+    };
+
+    return (
+      <Provider store={store2}>
+        <DocProvider content={props.content}>
+          <HtmlClassNameProvider className={docHtmlClassName}>
+            <DocItemMetadata />
+            <DocItemLayout>
+              <DocContent />
+            </DocItemLayout>
+          </HtmlClassNameProvider>
+        </DocProvider>
+      </Provider>
     );
   };
-
-  return (
-    <Provider store={store2}>
-      <DocProvider content={props.content}>
-        <HtmlClassNameProvider className={docHtmlClassName}>
-          <DocItemMetadata />
-          <DocItemLayout>
-            <DocContent />
-          </DocItemLayout>
-        </HtmlClassNameProvider>
-      </DocProvider>
-    </Provider>
-  );
 }
+
+export default DocItem;
