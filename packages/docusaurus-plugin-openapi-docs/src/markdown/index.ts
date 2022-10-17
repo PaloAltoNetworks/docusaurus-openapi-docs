6d2761567936dcf31f12fbad3f6b30f36e219eda
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-10-17 15:24:11 -0400

    Selective escape only non-HTML tags

--- packages/docusaurus-plugin-openapi-docs/src/markdown/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/index.ts
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-import { escape } from "lodash";
-
 import {
   ContactObject,
   LicenseObject,
@@ -26,6 +24,12 @@ import { createStatusCodes } from "./createStatusCodes";
 import { createTermsOfService } from "./createTermsOfService";
 import { createVersionBadge } from "./createVersionBadge";
 import { render } from "./utils";
+
+// Regex to selectively URL-encode '>' and '<' chars
+const lessThan =
+  /<(?!(hr|\s?\/hr|br|\s?\/br|span|\s?\/span|strong|\s?\/strong|small|\s?\/small|table|\s?\/table|td|\s?\/td|tr|\s?\/tr|th|\s?\/th|h1|\s?\/h1|h2|\s?\/h2|h3|\s?\/h3|h4|\s?\/h4|h5|\s?\/h5|h6|\s?\/h6|title|\s?\/title|p|\s?\/p|em|\s?\/em|b|\s?\/b|i|\s?\/i|u|\s?\/u|strike|\s?\/strike|a|\s?\/a|li|\s?\/li|ol|\s?\/ol|ul|\s?\/ul|img|\s?\/img|div|\s?\/div|center|\s?\/center))/giu;
+const greaterThan =
+  /(?<!(hr|br|span|strong|small|table|td|tr|th|h1|h2|h3|h4|h5|h6|title|p|em|b|i|u|strike|tag|li|ol|ul|img|div|center))>/giu;
 
 interface Props {
   title: string;
@@ -58,9 +62,11 @@ export function createApiPageMD({
     `import SchemaTabs from "@theme/SchemaTabs";\n`,
     `import DiscriminatorTabs from "@theme/DiscriminatorTabs";\n`,
     `import TabItem from "@theme/TabItem";\n\n`,
-    `## ${escape(title)}\n\n`,
+    `## ${title.replace(lessThan, "&lt;").replace(greaterThan, "&gt;")}\n\n`,
     createDeprecationNotice({ deprecated, description: deprecatedDescription }),
-    createDescription(escape(description)),
+    createDescription(
+      description?.replace(lessThan, "&lt;").replace(greaterThan, "&gt;")
+    ),
     createParamsDetails({ parameters, type: "path" }),
     createParamsDetails({ parameters, type: "query" }),
     createParamsDetails({ parameters, type: "header" }),
@@ -92,9 +98,11 @@ export function createInfoPageMD({
     `import TabItem from "@theme/TabItem";\n\n`,
 
     createVersionBadge(version),
-    `# ${escape(title)}\n\n`,
+    `# ${title.replace(lessThan, "&lt;").replace(greaterThan, "&gt;")}\n\n`,
     createLogo(logo, darkLogo),
-    createDescription(description),
+    createDescription(
+      description?.replace(lessThan, "&lt;").replace(greaterThan, "&gt;")
+    ),
     createAuthentication(securitySchemes as unknown as SecuritySchemeObject),
     createContactInfo(contact as ContactObject),
     createTermsOfService(termsOfService),
@@ -103,5 +111,9 @@ export function createInfoPageMD({
 }
 
 export function createTagPageMD({ tag: { description } }: TagPageMetadata) {
-  return render([createDescription(description)]);
+  return render([
+    createDescription(
+      description?.replace(lessThan, "&lt;").replace(greaterThan, "&gt;")
+    ),
+  ]);
 }
