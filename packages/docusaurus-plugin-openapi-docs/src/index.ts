9e945f4dc2813a5cab4dd1a97e4f790f888fa883
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-10-19 09:26:50 -0400

    Remove unused import

--- packages/docusaurus-plugin-openapi-docs/src/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/index.ts
@@ -8,7 +8,6 @@
 import fs from "fs";
 import path from "path";
 
-import plugin from "@docusaurus/mdx-loader/lib/remark/admonitions";
 import type { LoadContext, Plugin } from "@docusaurus/types";
 import { Globby } from "@docusaurus/utils";
 import chalk from "chalk";
