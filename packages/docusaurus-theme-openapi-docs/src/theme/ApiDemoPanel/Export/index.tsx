436d22dbe0f69e2893b5b60a0158bee08f5c0e9a
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-10-20 12:16:56 -0400

    Add Export component to theme

---
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/Export/index.tsx
@@ -0,0 +1,42 @@
+/* ============================================================================
+ * Copyright (c) Palo Alto Networks
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
+import React from "react";
+
+// @ts-ignore
+import fileSaver from "file-saver";
+
+const saveFile = (url: string) => {
+  fileSaver.saveAs(url, url.endsWith("json") ? "openapi.json" : "openapi.yaml");
+};
+
+function Export({ url, proxy }: any) {
+  return (
+    <div
+      style={{ float: "right" }}
+      className="dropdown dropdown--hoverable dropdown--right"
+    >
+      <button className="button button--sm button--secondary">Export 💾</button>
+      <ul className="dropdown__menu">
+        <li>
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              saveFile(`${url}`);
+            }}
+            className="dropdown__link"
+            href={`${url}`}
+          >
+            OpenAPI Spec
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Export;
