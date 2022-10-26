1e05dd0daf6fe880a4541423fcfa0d20fff22742
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-10-26 15:36:40 -0400

    Add createStoreWithoutState

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/store.ts
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiItem/store.ts
@@ -38,4 +38,15 @@ export const createStoreWithState = (
       getDefaultMiddleware().concat(...middlewares),
   });
 
+export const createStoreWithoutState = (
+  preloadedState: {},
+  middlewares: any[]
+) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(...middlewares),
+  });
+
 export type AppDispatch = ReturnType<typeof createStoreWithState>["dispatch"];
