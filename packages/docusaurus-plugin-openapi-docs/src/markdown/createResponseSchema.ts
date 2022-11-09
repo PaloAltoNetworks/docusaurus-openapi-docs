0fd44b530f9baf0eac03b090d9b2f63334b392de
XvladQt (Vladyslav Volynets) <Xvladxtreme@gmail.com>
2022-11-09 23:13:21 +0200

    Fix error when allOf two refs (#335)
    * Fix error when allOf two refs
    
    * fix: run prettier

--- packages/docusaurus-plugin-openapi-docs/src/markdown/createResponseSchema.ts
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/createResponseSchema.ts
@@ -31,6 +31,9 @@ export function mergeAllOf(allOf: SchemaObject[]) {
       example: function () {
         return true;
       },
+      "x-examples": function () {
+        return true;
+      },
     },
     ignoreAdditionalProperties: true,
   });
