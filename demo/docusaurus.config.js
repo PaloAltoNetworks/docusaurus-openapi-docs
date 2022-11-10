7796002dfc5382eafac8a221075e68d9641fba4a
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-11-10 15:14:02 -0500

    Switch to es-build

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -277,6 +277,16 @@ const config = {
       type: "text/css",
     },
   ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve("esbuild-loader"),
+      options: {
+        loader: "tsx",
+        format: isServer ? "cjs" : undefined,
+        target: isServer ? "node12" : "es2017",
+      },
+    }),
+  },
 };
 
 module.exports = config;
