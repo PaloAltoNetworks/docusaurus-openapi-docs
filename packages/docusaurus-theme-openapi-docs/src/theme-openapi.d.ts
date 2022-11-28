ed1d9b4bf34f956333f0c2dc5b89f0479225e9a7
Steven Serrata <sserrata@paloaltonetworks.com>
2022-11-28 16:23:52 -0500

    Add declarations for theme components

--- packages/docusaurus-theme-openapi-docs/src/theme-openapi.d.ts
+++ packages/docusaurus-theme-openapi-docs/src/theme-openapi.d.ts
@@ -46,3 +46,120 @@ declare module "@docusaurus/plugin-content-docs-types" {
 declare module "docusaurus-theme-openapi-docs" {
   export type ThemeConfig = Partial<import("./types").ThemeConfig>;
 }
+
+declare module "@theme/ApiItem/hooks" {
+  export { useTypedDispatch, useTypedSelector };
+}
+
+declare module "@theme/SchemaTabs" {
+  export default function SchemaTabs(props: any): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Accept" {
+  export default function Accept(): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Authorization" {
+  export default function Authorization(): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Body" {
+  import { Props as BodyProps } from "@theme/ApiDemoPanel/Body";
+  export default function Body(props: BodyProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Body/json2xml" {
+  export default function json2xml(any, any?): any;
+}
+
+declare module "@theme/ApiDemoPanel/CodeTabs" {
+  import { Props as CodeTabsProps } from "@theme/ApiDemoPanel/CodeTabs";
+  export default function CodeTabs(props: CodeTabsProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/ContentType" {
+  export default function ContentType(): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Curl" {
+  import { Props as CurlProps } from "@theme/ApiDemoPanel/Curl";
+  export { languageSet, Language } from "@theme/ApiDemoPanel/Curl";
+  export default function Curl(props: CurlProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FloatingButton" {
+  import { Props as FloatingButtonProps } from "@theme/ApiDemoPanel/FloatingButton";
+  export default function FloatingButton(
+    props: FloatingButtonProps
+  ): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FormItem" {
+  import { Props as FormItemProps } from "@theme/ApiDemoPanel/FormItem";
+  export default function FormItem(props: FormItemProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FormSelect" {
+  import { Props as FormSelectProps } from "@theme/ApiDemoPanel/FormSelect";
+  export default function FormSelect(props: FormSelectProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FormTextInput" {
+  import { Props as FormTextInputProps } from "@theme/ApiDemoPanel/FormTextInput";
+  export default function FormTextInput(props: FormTextInputProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FormFileUpload" {
+  import { Props as FormFileUploadProps } from "@theme/ApiDemoPanel/FormFileUpload";
+  export default function FormFileUpload(
+    props: FormFileUploadProps
+  ): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/FormMultiSelect" {
+  import { Props as FormMultiSelectProps } from "@theme/ApiDemoPanel/FormMultiSelect";
+  export default function FormMultiSelect(
+    props: FormMultiSelectProps
+  ): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Execute" {
+  import { Props as ExecuteProps } from "@theme/ApiDemoPanel/Execute";
+  export default function Execute(props: ExecuteProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/LiveEditor" {
+  export default function LiveEditor(props: any): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/MethodEndpoint" {
+  import { Props as MethodEndpointProps } from "@theme/ApiDemoPanel/MethodEndpoint";
+  export default function MethodEndpoint(
+    props: MethodEndpointProps
+  ): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/ParamOptions" {
+  import { ParamProps } from "@theme/ApiDemoPanel/ParamOptions";
+  export default function ParamOptions(props: ParamProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Request" {
+  import { ApiItem } from "docusaurus-plugin-openapi-docs/src/types";
+  export interface RequestProps {
+    item: NonNullable<ApiItem>;
+  }
+  export default function Request(props: RequestProps): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Response" {
+  export default function Response(): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/SecuritySchemes" {
+  export default function SecuritySchemes(props: any): JSX.Element;
+}
+
+declare module "@theme/ApiDemoPanel/Server" {
+  export default function Server(): JSX.Element;
+}
