f06cfcb5ae182bc45e9d797646f8222e7d5ed61a
blindaa121 <blindaa121@gmail.com>
2023-02-13 15:05:45 -0800

    Remove MethodEndpoint from ApiDemoPanel

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/index.tsx
@@ -9,7 +9,6 @@ import React from "react";
 
 import sdk from "@paloaltonetworks/postman-collection";
 import Curl from "@theme/ApiDemoPanel/Curl";
-import MethodEndpoint from "@theme/ApiDemoPanel/MethodEndpoint";
 import Request from "@theme/ApiDemoPanel/Request";
 import Response from "@theme/ApiDemoPanel/Response";
 import SecuritySchemes from "@theme/ApiDemoPanel/SecuritySchemes";
@@ -27,7 +26,6 @@ function ApiDemoPanel({
 
   return (
     <div>
-      <MethodEndpoint method={method} path={path} />
       <SecuritySchemes infoPath={infoPath} />
       <Request item={item} />
       <Response />
