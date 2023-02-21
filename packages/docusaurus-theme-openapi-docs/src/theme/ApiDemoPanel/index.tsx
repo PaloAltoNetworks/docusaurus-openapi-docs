a8e0b7e431f6905871fae718c3fb6fc2ef04f69a
Bryan Linda <48506502+blindaa121@users.noreply.github.com>
2023-02-21 12:29:02 -0800

    [UI Enhancement] Move MethodEndpoint from ApiDemoPanel to left doc panel (#429)
    * Add v2 announcement bar
    
    * Remove MethodEndpoint from ApiDemoPanel
    
    * Move MethodEndpoint into api page
    
    * Add newline underneath deprecation notice
    
    * Fix linting issues
    
    * Render MethodEndpoint directly below title
    
    * Resolve merge conflict
    
    * Use width:fit-content for method endpoint
    
    * Add server url to method endpoint
    
    * Fix linting issues
    
    * Add max-width to prevent overflow
    
    * BrowserOnly wrapper test for method endpoint
    
    * Fix linting issues
    
    * Add server variables support
    
    * Refactor server url render with BrowserOnly
    
    * Move serverValue and serverUrlWithVariables to MethodEndpoint component level

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/index.tsx
@@ -9,7 +9,6 @@ import React from "react";
 
 import sdk from "@paloaltonetworks/postman-collection";
 import Curl from "@theme/ApiDemoPanel/Curl";
-import MethodEndpoint from "@theme/ApiDemoPanel/MethodEndpoint";
 import Request from "@theme/ApiDemoPanel/Request";
 import Response from "@theme/ApiDemoPanel/Response";
 import SecuritySchemes from "@theme/ApiDemoPanel/SecuritySchemes";
@@ -23,11 +22,9 @@ function ApiDemoPanel({
   infoPath: string;
 }) {
   const postman = new sdk.Request(item.postman);
-  const { path, method } = item;
 
   return (
     <div>
-      <MethodEndpoint method={method} path={path} />
       <SecuritySchemes infoPath={infoPath} />
       <Request item={item} />
       <Response />
