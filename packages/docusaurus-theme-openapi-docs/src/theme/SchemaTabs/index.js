b80e22461367bff0a1b3dd0805d37ebf2ea6d409
Steven Serrata <sserrata@paloaltonetworks.com>
2023-03-01 11:58:40 -0500

    Fix linter issues

--- packages/docusaurus-theme-openapi-docs/src/theme/SchemaTabs/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/SchemaTabs/index.js
@@ -1,11 +1,21 @@
+/* ============================================================================
+ * Copyright (c) Palo Alto Networks
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
 import React, { cloneElement } from "react";
-import clsx from "clsx";
+
 import {
   useScrollPositionBlocker,
   useTabs,
 } from "@docusaurus/theme-common/internal";
 import useIsBrowser from "@docusaurus/useIsBrowser";
+import clsx from "clsx";
+
 import styles from "./styles.module.css";
+
 function TabList({ className, block, selectedValue, selectValue, tabValues }) {
   const tabRefs = [];
   const { blockElementScrollPositionUntilNextRender } =
