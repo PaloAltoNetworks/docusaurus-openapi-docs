a8cca565f4bcc8705818ed221a95e60b5fe20137
Steven Serrata <sserrata@paloaltonetworks.com>
2023-03-17 10:28:07 -0400

    Add title to code blocks

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/Curl/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/Curl/index.tsx
@@ -290,7 +290,11 @@ function Curl({ postman, codeSamples }: Props) {
               }
               attributes={{ className: `code__tab--${lang.logoClass}` }}
             >
-              <CodeBlock language={lang.highlight} className={styles.codeBlock}>
+              <CodeBlock
+                language={lang.highlight}
+                className={styles.codeBlock}
+                title={`${lang.language} / ${lang.variant}`}
+              >
                 {codeText}
               </CodeBlock>
             </CodeTab>
