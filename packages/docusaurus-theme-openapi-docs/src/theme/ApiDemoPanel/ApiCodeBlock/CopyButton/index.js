a2e0264857477914b0e5a28b099954d7f7c051dc
Bryan <blindaa121@gmail.com>
2023-04-05 20:17:52 -0700

    Migrate ApiCodeBlock/CopyButton to scss

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/CopyButton/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/CopyButton/index.js
@@ -12,8 +12,6 @@ import clsx from "clsx";
 // @ts-expect-error: TODO, we need to make theme-classic have type: module
 import copy from "copy-text-to-clipboard";
 
-import styles from "./styles.module.css";
-
 export default function CopyButton({ code, className }) {
   const [isCopied, setIsCopied] = useState(false);
   const copyTimeout = useRef(undefined);
@@ -49,16 +47,25 @@ export default function CopyButton({ code, className }) {
       className={clsx(
         "clean-btn",
         className,
-        styles.copyButton,
-        isCopied && styles.copyButtonCopied
+        "openapi-demo__code-block-copy-btn",
+        isCopied && "openapi-demo__code-block-copy-btn--copied"
       )}
       onClick={handleCopyCode}
     >
-      <span className={styles.copyButtonIcons} aria-hidden="true">
-        <svg className={styles.copyButtonIcon} viewBox="0 0 24 24">
+      <span
+        className="openapi-demo__code-block-copy-btn-icons"
+        aria-hidden="true"
+      >
+        <svg
+          className="openapi-demo__code-block-copy-btn-icon"
+          viewBox="0 0 24 24"
+        >
           <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
         </svg>
-        <svg className={styles.copyButtonSuccessIcon} viewBox="0 0 24 24">
+        <svg
+          className="openapi-demo__code-block-copy-btn-icon--success"
+          viewBox="0 0 24 24"
+        >
           <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
         </svg>
       </span>
