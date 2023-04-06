9c7669a98678954c419f4f30d28303b5484abfee
Bryan <blindaa121@gmail.com>
2023-04-05 19:48:01 -0700

    Migrate ApiCodeBlock Container styles to scss

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/Container/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/Container/index.js
@@ -11,8 +11,6 @@ import { ThemeClassNames, usePrismTheme } from "@docusaurus/theme-common";
 import { getPrismCssVariables } from "@docusaurus/theme-common/internal";
 import clsx from "clsx";
 
-import styles from "./styles.module.css";
-
 export default function CodeBlockContainer({ as: As, ...props }) {
   const prismTheme = usePrismTheme();
   const prismCssVariables = getPrismCssVariables(prismTheme);
@@ -22,8 +20,8 @@ export default function CodeBlockContainer({ as: As, ...props }) {
       {...props}
       style={prismCssVariables}
       className={clsx(
+        "openapi-demo__code-block-container",
         props.className,
-        styles.codeBlockContainer,
         ThemeClassNames.common.codeBlock
       )}
     />
