462576d23c25529016e2ed584b716149a6fce582
Bryan <blindaa121@gmail.com>
2023-04-05 20:26:29 -0700

    Migrate ApiCodeBlock/ExitButton styles to scss

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/ExitButton/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ApiCodeBlock/ExitButton/index.js
@@ -10,8 +10,6 @@ import React from "react";
 import { translate } from "@docusaurus/Translate";
 import clsx from "clsx";
 
-import styles from "./styles.module.css";
-
 export default function ExitButton({ className, handler }) {
   return (
     <button
@@ -26,11 +24,21 @@ export default function ExitButton({ className, handler }) {
         message: "Copy",
         description: "The exit button label on code blocks",
       })}
-      className={clsx("clean-btn", className, styles.exitButton)}
+      className={clsx(
+        "clean-btn",
+        "openapi-demo__code-block-exit-btn",
+        className
+      )}
       onClick={handler}
     >
-      <span className={styles.exitButtonIcons} aria-hidden="true">
-        <svg className={styles.exitButtonIcon} viewBox="0 0 384 512">
+      <span
+        className="openapi-demo__code-block-exit-btn-icons"
+        aria-hidden="true"
+      >
+        <svg
+          className="openapi-demo__code-block-exit-btn-icon"
+          viewBox="0 0 384 512"
+        >
           <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
         </svg>
       </span>
