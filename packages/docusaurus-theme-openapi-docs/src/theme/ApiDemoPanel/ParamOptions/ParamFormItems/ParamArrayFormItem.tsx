edef58d8bbf2ab804bb4d8c81df24b87ddafbf14
Bryan <blindaa121@gmail.com>
2023-04-19 13:39:01 -0700

    Extend validations to ParamArrayFormItem

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ParamOptions/ParamFormItems/ParamArrayFormItem.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/ParamOptions/ParamFormItems/ParamArrayFormItem.tsx
@@ -8,10 +8,12 @@
 import React, { useEffect, useState } from "react";
 
 import { nanoid } from "@reduxjs/toolkit";
+import { ErrorMessage } from "@hookform/error-message";
 import FormSelect from "@theme/ApiDemoPanel/FormSelect";
 import FormTextInput from "@theme/ApiDemoPanel/FormTextInput";
 import { Param, setParam } from "@theme/ApiDemoPanel/ParamOptions/slice";
 import { useTypedDispatch } from "@theme/ApiItem/hooks";
+import { Controller, useFormContext } from "react-hook-form";
 
 export interface ParamProps {
   param: Param;
@@ -35,7 +37,6 @@ function ArrayItem({
 
   return (
     <FormTextInput
-      // {...register(`${param.name}`)}
       placeholder={param.description || param.name}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         onChange(e.target.value);
@@ -47,6 +48,13 @@ function ArrayItem({
 export default function ParamArrayFormItem({ param }: ParamProps) {
   const [items, setItems] = useState<{ id: string; value?: string }[]>([]);
   const dispatch = useTypedDispatch();
+
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  const showErrorMessage = errors?.paramArray?.message;
 
   function handleAddItem() {
     setItems((i) => [
@@ -78,7 +86,7 @@ export default function ParamArrayFormItem({ param }: ParamProps) {
     };
   }
 
-  function handleChangeItem(itemToUpdate: { id: string }) {
+  function handleChangeItem(itemToUpdate: { id: string }, onChange: any) {
     return (value: string) => {
       const newItems = items.map((i) => {
         if (i.id === itemToUpdate.id) {
@@ -87,37 +95,59 @@ export default function ParamArrayFormItem({ param }: ParamProps) {
         return i;
       });
       setItems(newItems);
+      onChange(newItems);
     };
   }
 
   return (
     <>
-      {items.map((item) => (
-        <div key={item.id} style={{ display: "flex" }}>
-          <ArrayItem param={param} onChange={handleChangeItem(item)} />
-          <button
-            className="openapi-demo__delete-btn"
-            onClick={handleDeleteItem(item)}
-          >
-            <svg
-              focusable="false"
-              preserveAspectRatio="xMidYMid meet"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              width="16"
-              height="16"
-              viewBox="0 0 32 32"
-              aria-hidden="true"
-            >
-              <path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4z"></path>
-              <title>Delete</title>
-            </svg>
-          </button>
-        </div>
-      ))}
-      <button className="openapi-demo__thin-btn" onClick={handleAddItem}>
-        Add item
-      </button>
+      <Controller
+        control={control}
+        rules={{ required: param.required ? "This field is required" : false }}
+        name="paramArray"
+        render={({ field: { onChange, name } }) => (
+          <>
+            {items.map((item) => (
+              <div key={item.id} style={{ display: "flex" }}>
+                <ArrayItem
+                  param={param}
+                  onChange={handleChangeItem(item, onChange)}
+                />
+                <button
+                  className="openapi-demo__delete-btn"
+                  onClick={handleDeleteItem(item)}
+                >
+                  <svg
+                    focusable="false"
+                    preserveAspectRatio="xMidYMid meet"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 32 32"
+                    aria-hidden="true"
+                  >
+                    <path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4z"></path>
+                    <title>Delete</title>
+                  </svg>
+                </button>
+              </div>
+            ))}
+            <button className="openapi-demo__thin-btn" onClick={handleAddItem}>
+              Add item
+            </button>
+          </>
+        )}
+      />
+      {showErrorMessage && (
+        <ErrorMessage
+          errors={errors}
+          name="paramArray"
+          render={({ message }) => (
+            <div className="openapi-demo__input-error">{message}</div>
+          )}
+        />
+      )}
     </>
   );
 }
