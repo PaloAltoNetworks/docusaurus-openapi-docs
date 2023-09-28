3bb9bdc269cf5fb95364ae499d7c673ce38f8a45
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2023-09-28 09:27:44 +0200

    Test oneOf tabs showing correct information

---
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/createSchema.test.ts
@@ -0,0 +1,56 @@
+/* ============================================================================
+ * Copyright (c) Palo Alto Networks
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * ========================================================================== */
+
+import * as prettier from "prettier";
+
+import { SchemaObject } from "../openapi/types";
+import { createNodes } from "./createSchema";
+
+describe("createNodes", () => {
+  it("should create readable MODs for oneOf primitive properties", () => {
+    const schema: SchemaObject = {
+      type: "object",
+      properties: {
+        oneOfProperty: {
+          oneOf: [
+            {
+              type: "object",
+              properties: {
+                noseLength: {
+                  type: "number",
+                },
+              },
+              required: ["noseLength"],
+              description: "Clown's nose length",
+            },
+            {
+              type: "array",
+              items: {
+                type: "string",
+              },
+              description: "Array of strings",
+            },
+            {
+              type: "boolean",
+            },
+            {
+              type: "number",
+            },
+            {
+              type: "string",
+            },
+          ],
+        },
+      },
+    };
+    expect(
+      createNodes(schema).map((md: any) =>
+        prettier.format(md, { parser: "babel" })
+      )
+    ).toMatchSnapshot();
+  });
+});
