b70cba5d1d4c75a6defafde3e44b610d444be4d5
Steven Serrata <sserrata@paloaltonetworks.com>
2024-01-08 08:25:36 -0500

    fix import order

--- packages/docusaurus-theme-openapi-docs/src/theme/Markdown/index.js
+++ packages/docusaurus-theme-openapi-docs/src/theme/Markdown/index.js
@@ -7,13 +7,29 @@
 
 import React from "react";
 
+import CodeBlock from "@theme/CodeBlock";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 
 function Markdown({ children }) {
   return (
     <div>
-      <ReactMarkdown children={children} rehypePlugins={[rehypeRaw]} />
+      <ReactMarkdown
+        children={children}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          pre: "div",
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            if (inline) return <code>{children}</code>;
+            return !inline && match ? (
+              <CodeBlock className={className}>{children}</CodeBlock>
+            ) : (
+              <CodeBlock>{children}</CodeBlock>
+            );
+          },
+        }}
+      />
     </div>
   );
 }
