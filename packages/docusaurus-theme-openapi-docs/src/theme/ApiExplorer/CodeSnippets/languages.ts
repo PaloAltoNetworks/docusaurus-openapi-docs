9367911580ef23fe65dccf2ca433ea45d3c572ea
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2024-07-02 14:34:55 -0500

    cleanup ts config

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiExplorer/CodeSnippets/languages.ts
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiExplorer/CodeSnippets/languages.ts
@@ -5,21 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
+import find from "lodash/find";
+import isArray from "lodash/isArray";
+import mergeWith from "lodash/mergeWith";
+import unionBy from "lodash/unionBy";
+
 import { CodeSample, Language } from "./code-snippets-types";
 
 export function mergeCodeSampleLanguage(
   languages: Language[],
-  codeSamples: CodeSample[]
+  codeSamples: CodeSample[],
 ): Language[] {
   return languages.map((language) => {
     const languageCodeSamples = codeSamples.filter(
-      ({ lang }) => lang === language.codeSampleLanguage
+      ({ lang }) => lang === language.codeSampleLanguage,
     );
 
     if (languageCodeSamples.length) {
       const samples = languageCodeSamples.map(({ lang }) => lang);
       const samplesLabels = languageCodeSamples.map(
-        ({ label, lang }) => label || lang
+        ({ label, lang }) => label || lang,
       );
       const samplesSources = languageCodeSamples.map(({ source }) => source);
 
@@ -36,6 +41,23 @@ export function mergeCodeSampleLanguage(
   });
 }
 
+export const mergeArraysbyLanguage = (arr1: any, arr2: any) => {
+  const mergedArray = unionBy(arr1, arr2, "language");
+
+  return mergedArray.map((item: any) => {
+    const matchingItems = [
+      find(arr1, ["language", item["language"]]),
+      find(arr2, ["language", item["language"]]),
+    ];
+    return mergeWith({}, ...matchingItems, (objValue: any) => {
+      if (isArray(objValue)) {
+        return objValue;
+      }
+      return undefined;
+    });
+  });
+};
+
 export function getCodeSampleSourceFromLanguage(language: Language) {
   if (
     language &&
@@ -44,7 +66,7 @@ export function getCodeSampleSourceFromLanguage(language: Language) {
     language.samplesSources
   ) {
     const sampleIndex = language.samples.findIndex(
-      (smp) => smp === language.sample
+      (smp) => smp === language.sample,
     );
     return language.samplesSources[sampleIndex];
   }
