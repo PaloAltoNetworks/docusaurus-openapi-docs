febf5a1a55f6cd8a0581acd65101c4ee0040c88a
Steven Serrata <sserrata@paloaltonetworks.com>
2024-07-02 17:15:35 -0500

    update isURL function to support abs path

--- packages/docusaurus-plugin-openapi-docs/src/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/index.ts
@@ -35,8 +35,8 @@ import type {
   SchemaPageMetadata,
 } from "./types";
 
-export function isURL(str: string): boolean {
-  return /^(https?:)\/\//m.test(str);
+export function isUrlOrPath(str: string): boolean {
+  return /^(https?:\/\/|\/[\w-]+).*$/i.test(str);
 }
 
 export function getDocsPluginConfig(
@@ -133,7 +133,7 @@ export default function pluginOpenAPIDocs(
       docPath = docData ? (docData.path ? docData.path : "docs") : undefined;
     }
 
-    const contentPath = isURL(specPath)
+    const contentPath = isUrlOrPath(specPath)
       ? specPath
       : path.resolve(siteDir, specPath);
 
@@ -312,7 +312,7 @@ custom_edit_url: null
 
       loadedApi.map(async (item) => {
         if (item.type === "info") {
-          if (downloadUrl && isURL(downloadUrl)) {
+          if (downloadUrl && isUrlOrPath(downloadUrl)) {
             item.downloadUrl = downloadUrl;
           }
         }
