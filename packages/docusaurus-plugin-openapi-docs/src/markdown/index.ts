0a83e7936c90c10fb625dd056597bc9c6e34f4c0
Steven Serrata <sserrata@paloaltonetworks.com>
2024-07-23 14:28:18 -0500

    remove Markdown import from api page template

--- packages/docusaurus-plugin-openapi-docs/src/markdown/index.ts
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/index.ts
@@ -76,7 +76,6 @@ export function createApiPageMD({
     `import ResponseSamples from "@theme/ResponseSamples";\n`,
     `import SchemaItem from "@theme/SchemaItem";\n`,
     `import SchemaTabs from "@theme/SchemaTabs";\n`,
-    `import Markdown from "@theme/Markdown";\n`,
     `import Heading from "@theme/Heading";\n`,
     `import OperationTabs from "@theme/OperationTabs";\n`,
     `import TabItem from "@theme/TabItem";\n\n`,
