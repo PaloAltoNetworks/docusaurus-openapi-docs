247e7f1438aab4386b99e4d2fc8723919e1b7595
Steven Serrata <sserrata@paloaltonetworks.com>
2024-09-28 19:00:05 -0400

    refactor createParamsDetails to use component

--- packages/docusaurus-plugin-openapi-docs/src/markdown/createParamsDetails.ts
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/createParamsDetails.ts
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-import { createDetails } from "./createDetails";
-import { createDetailsSummary } from "./createDetailsSummary";
 import { create } from "./utils";
 import { ApiItem } from "../types";
 
@@ -16,49 +14,11 @@ interface Props {
 }
 
 export function createParamsDetails({ parameters, type }: Props) {
-  if (parameters === undefined) {
-    return undefined;
-  }
-  const params = parameters.filter((param) => param?.in === type);
-  if (params.length === 0) {
-    return undefined;
-  }
-
-  return createDetails({
-    className: "openapi-markdown__details",
-    "data-collapsed": false,
-    open: true,
-    style: { marginBottom: "1rem" },
-    children: [
-      createDetailsSummary({
-        children: [
-          create("h3", {
-            className: "openapi-markdown__details-summary-header-params",
-            children: `${
-              type.charAt(0).toUpperCase() + type.slice(1)
-            } Parameters`,
-          }),
-        ],
-      }),
-      create("div", {
-        children: [
-          create("ul", {
-            children: params.map((param) => {
-              return create("ParamsItem", {
-                className: "paramsItem",
-                param: {
-                  ...param,
-                  enumDescriptions: Object.entries(
-                    param?.schema?.["x-enumDescriptions"] ??
-                      param?.schema?.items?.["x-enumDescriptions"] ??
-                      {}
-                  ),
-                },
-              });
-            }),
-          }),
-        ],
-      }),
-    ],
-  });
+  return [
+    create("ParamsDetails", {
+      parameters: parameters,
+      type: type,
+    }),
+    "\n\n",
+  ];
 }
