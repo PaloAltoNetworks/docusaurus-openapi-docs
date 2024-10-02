257a59bbf21376320cbad313d2d7c1e01bb1cf1d
Steven Serrata <sserrata@paloaltonetworks.com>
2024-10-02 11:26:51 -0400

    add StatusCodes component

--- packages/docusaurus-plugin-openapi-docs/src/markdown/createStatusCodes.ts
+++ packages/docusaurus-plugin-openapi-docs/src/markdown/createStatusCodes.ts
@@ -5,15 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-import format from "xml-formatter";
-
-import { createDescription } from "./createDescription";
-import { createDetails } from "./createDetails";
-import { createDetailsSummary } from "./createDetailsSummary";
-import { createResponseSchema } from "./createResponseSchema";
 import { create } from "./utils";
-import { guard } from "./utils";
-import { sampleResponseFromSchema } from "../openapi/createResponseExample";
 import { ApiItem } from "../types";
 
 export default function json2xml(o: any, tab: any) {
@@ -59,264 +51,275 @@ interface Props {
   responses: ApiItem["responses"];
 }
 
-function createResponseHeaders(responseHeaders: any) {
-  return guard(responseHeaders, () =>
-    create("ul", {
-      style: { marginLeft: "1rem" },
-      children: [
-        Object.entries(responseHeaders).map(
-          ([headerName, headerObj]: [any, any]) => {
-            const { description, example }: any = headerObj;
-            const type = headerObj.schema?.type ?? "any";
-
-            return create("li", {
-              className: "schemaItem",
-              children: [
-                createDetailsSummary({
-                  children: [
-                    create("strong", { children: headerName }),
-                    guard(type, () => [
-                      create("span", {
-                        style: { opacity: "0.6" },
-                        children: ` ${type}`,
-                      }),
-                    ]),
-                  ],
-                }),
-                create("div", {
-                  children: [
-                    guard(description, (description) =>
-                      create("div", {
-                        style: {
-                          marginTop: ".5rem",
-                          marginBottom: ".5rem",
-                        },
-                        children: [
-                          guard(example, () => `Example: ${example}`),
-                          createDescription(description),
-                        ],
-                      })
-                    ),
-                  ],
-                }),
-              ],
-            });
-          }
-        ),
-      ],
-    })
-  );
+export function createStatusCodes({ id, label, responses }: Props) {
+  return [
+    create("StatusCodes", {
+      id: id,
+      label: label,
+      responses: responses,
+    }),
+    "\n\n",
+  ];
 }
 
-export function createResponseExamples(
-  responseExamples: any,
-  mimeType: string
-) {
-  let language = "shell";
-  if (mimeType.endsWith("json")) {
-    language = "json";
-  }
-  if (mimeType.endsWith("xml")) {
-    language = "xml";
-  }
-  return Object.entries(responseExamples).map(
-    ([exampleName, exampleValue]: any) => {
-      if (typeof exampleValue.value === "object") {
-        return create("TabItem", {
-          label: `${exampleName}`,
-          value: `${exampleName}`,
-          children: [
-            guard(exampleValue.summary, (summary) => [
-              create("div", {
-                children: `${summary}`,
-                className: "openapi-example__summary",
-              }),
-            ]),
-            create("ResponseSamples", {
-              responseExample: JSON.stringify(exampleValue.value, null, 2),
-              language: language,
-            }),
-          ],
-        });
-      }
-      return create("TabItem", {
-        label: `${exampleName}`,
-        value: `${exampleName}`,
-        children: [
-          guard(exampleValue.summary, (summary) => [
-            create("div", {
-              children: `${summary}`,
-              className: "openapi-example__summary",
-            }),
-          ]),
-          create("ResponseSamples", {
-            responseExample: exampleValue.value,
-            language: language,
-          }),
-        ],
-      });
-    }
-  );
-}
+// function createResponseHeaders(responseHeaders: any) {
+//   return guard(responseHeaders, () =>
+//     create("ul", {
+//       style: { marginLeft: "1rem" },
+//       children: [
+//         Object.entries(responseHeaders).map(
+//           ([headerName, headerObj]: [any, any]) => {
+//             const { description, example }: any = headerObj;
+//             const type = headerObj.schema?.type ?? "any";
 
-export function createResponseExample(responseExample: any, mimeType: string) {
-  let language = "shell";
-  if (mimeType.endsWith("json")) {
-    language = "json";
-  }
-  if (mimeType.endsWith("xml")) {
-    language = "xml";
-  }
-  if (typeof responseExample === "object") {
-    return create("TabItem", {
-      label: `Example`,
-      value: `Example`,
-      children: [
-        guard(responseExample.summary, (summary) => [
-          create("div", {
-            children: `${summary}`,
-            className: "openapi-example__summary",
-          }),
-        ]),
-        create("ResponseSamples", {
-          responseExample: JSON.stringify(responseExample, null, 2),
-          language: language,
-        }),
-      ],
-    });
-  }
-  return create("TabItem", {
-    label: `Example`,
-    value: `Example`,
-    children: [
-      guard(responseExample.summary, (summary) => [
-        create("div", {
-          children: `${summary}`,
-          className: "openapi-example__summary",
-        }),
-      ]),
-      create("ResponseSamples", {
-        responseExample: responseExample,
-        language: language,
-      }),
-    ],
-  });
-}
+//             return create("li", {
+//               className: "schemaItem",
+//               children: [
+//                 createDetailsSummary({
+//                   children: [
+//                     create("strong", { children: headerName }),
+//                     guard(type, () => [
+//                       create("span", {
+//                         style: { opacity: "0.6" },
+//                         children: ` ${type}`,
+//                       }),
+//                     ]),
+//                   ],
+//                 }),
+//                 create("div", {
+//                   children: [
+//                     guard(description, (description) =>
+//                       create("div", {
+//                         style: {
+//                           marginTop: ".5rem",
+//                           marginBottom: ".5rem",
+//                         },
+//                         children: [
+//                           guard(example, () => `Example: ${example}`),
+//                           createDescription(description),
+//                         ],
+//                       })
+//                     ),
+//                   ],
+//                 }),
+//               ],
+//             });
+//           }
+//         ),
+//       ],
+//     })
+//   );
+// }
 
-export function createExampleFromSchema(schema: any, mimeType: string) {
-  const responseExample = sampleResponseFromSchema(schema);
-  if (mimeType.endsWith("xml")) {
-    let responseExampleObject;
-    try {
-      responseExampleObject = JSON.parse(JSON.stringify(responseExample));
-    } catch {
-      return undefined;
-    }
+// export function createResponseExamples(
+//   responseExamples: any,
+//   mimeType: string
+// ) {
+//   let language = "shell";
+//   if (mimeType.endsWith("json")) {
+//     language = "json";
+//   }
+//   if (mimeType.endsWith("xml")) {
+//     language = "xml";
+//   }
+//   return Object.entries(responseExamples).map(
+//     ([exampleName, exampleValue]: any) => {
+//       if (typeof exampleValue.value === "object") {
+//         return create("TabItem", {
+//           label: `${exampleName}`,
+//           value: `${exampleName}`,
+//           children: [
+//             guard(exampleValue.summary, (summary) => [
+//               create("div", {
+//                 children: `${summary}`,
+//                 className: "openapi-example__summary",
+//               }),
+//             ]),
+//             create("ResponseSamples", {
+//               responseExample: JSON.stringify(exampleValue.value, null, 2),
+//               language: language,
+//             }),
+//           ],
+//         });
+//       }
+//       return create("TabItem", {
+//         label: `${exampleName}`,
+//         value: `${exampleName}`,
+//         children: [
+//           guard(exampleValue.summary, (summary) => [
+//             create("div", {
+//               children: `${summary}`,
+//               className: "openapi-example__summary",
+//             }),
+//           ]),
+//           create("ResponseSamples", {
+//             responseExample: exampleValue.value,
+//             language: language,
+//           }),
+//         ],
+//       });
+//     }
+//   );
+// }
 
-    if (typeof responseExampleObject === "object") {
-      let xmlExample;
-      try {
-        xmlExample = format(json2xml(responseExampleObject, ""), {
-          indentation: "  ",
-          lineSeparator: "\n",
-          collapseContent: true,
-        });
-      } catch {
-        const xmlExampleWithRoot = { root: responseExampleObject };
-        try {
-          xmlExample = format(json2xml(xmlExampleWithRoot, ""), {
-            indentation: "  ",
-            lineSeparator: "\n",
-            collapseContent: true,
-          });
-        } catch {
-          xmlExample = json2xml(responseExampleObject, "");
-        }
-      }
-      return create("TabItem", {
-        label: `Example (from schema)`,
-        value: `Example (from schema)`,
-        children: [
-          create("ResponseSamples", {
-            responseExample: xmlExample,
-            language: "xml",
-          }),
-        ],
-      });
-    }
-  }
-  if (typeof responseExample === "object") {
-    return create("TabItem", {
-      label: `Example (from schema)`,
-      value: `Example (from schema)`,
-      children: [
-        create("ResponseSamples", {
-          responseExample: JSON.stringify(responseExample, null, 2),
-          language: "json",
-        }),
-      ],
-    });
-  }
-  return undefined;
-}
+// export function createResponseExample(responseExample: any, mimeType: string) {
+//   let language = "shell";
+//   if (mimeType.endsWith("json")) {
+//     language = "json";
+//   }
+//   if (mimeType.endsWith("xml")) {
+//     language = "xml";
+//   }
+//   if (typeof responseExample === "object") {
+//     return create("TabItem", {
+//       label: `Example`,
+//       value: `Example`,
+//       children: [
+//         guard(responseExample.summary, (summary) => [
+//           create("div", {
+//             children: `${summary}`,
+//             className: "openapi-example__summary",
+//           }),
+//         ]),
+//         create("ResponseSamples", {
+//           responseExample: JSON.stringify(responseExample, null, 2),
+//           language: language,
+//         }),
+//       ],
+//     });
+//   }
+//   return create("TabItem", {
+//     label: `Example`,
+//     value: `Example`,
+//     children: [
+//       guard(responseExample.summary, (summary) => [
+//         create("div", {
+//           children: `${summary}`,
+//           className: "openapi-example__summary",
+//         }),
+//       ]),
+//       create("ResponseSamples", {
+//         responseExample: responseExample,
+//         language: language,
+//       }),
+//     ],
+//   });
+// }
 
-export function createStatusCodes({ label, id, responses }: Props) {
-  if (responses === undefined) {
-    return undefined;
-  }
+// export function createExampleFromSchema(schema: any, mimeType: string) {
+//   const responseExample = sampleResponseFromSchema(schema);
+//   if (mimeType.endsWith("xml")) {
+//     let responseExampleObject;
+//     try {
+//       responseExampleObject = JSON.parse(JSON.stringify(responseExample));
+//     } catch {
+//       return undefined;
+//     }
 
-  const codes = Object.keys(responses);
-  if (codes.length === 0) {
-    return undefined;
-  }
+//     if (typeof responseExampleObject === "object") {
+//       let xmlExample;
+//       try {
+//         xmlExample = format(json2xml(responseExampleObject, ""), {
+//           indentation: "  ",
+//           lineSeparator: "\n",
+//           collapseContent: true,
+//         });
+//       } catch {
+//         const xmlExampleWithRoot = { root: responseExampleObject };
+//         try {
+//           xmlExample = format(json2xml(xmlExampleWithRoot, ""), {
+//             indentation: "  ",
+//             lineSeparator: "\n",
+//             collapseContent: true,
+//           });
+//         } catch {
+//           xmlExample = json2xml(responseExampleObject, "");
+//         }
+//       }
+//       return create("TabItem", {
+//         label: `Example (from schema)`,
+//         value: `Example (from schema)`,
+//         children: [
+//           create("ResponseSamples", {
+//             responseExample: xmlExample,
+//             language: "xml",
+//           }),
+//         ],
+//       });
+//     }
+//   }
+//   if (typeof responseExample === "object") {
+//     return create("TabItem", {
+//       label: `Example (from schema)`,
+//       value: `Example (from schema)`,
+//       children: [
+//         create("ResponseSamples", {
+//           responseExample: JSON.stringify(responseExample, null, 2),
+//           language: "json",
+//         }),
+//       ],
+//     });
+//   }
+//   return undefined;
+// }
 
-  return create("div", {
-    children: [
-      create("div", {
-        children: [
-          create("ApiTabs", {
-            label,
-            id,
-            children: codes.map((code) => {
-              const responseHeaders: any = responses[code].headers;
-              return create("TabItem", {
-                label: code,
-                value: code,
-                children: [
-                  create("div", {
-                    children: createDescription(responses[code].description),
-                  }),
-                  responseHeaders &&
-                    createDetails({
-                      className: "openapi-markdown__details",
-                      "data-collapsed": true,
-                      open: false,
-                      style: { textAlign: "left", marginBottom: "1rem" },
-                      children: [
-                        createDetailsSummary({
-                          children: [
-                            create("strong", {
-                              children: "Response Headers",
-                            }),
-                          ],
-                        }),
-                        createResponseHeaders(responseHeaders),
-                      ],
-                    }),
-                  create("div", {
-                    children: createResponseSchema({
-                      title: "Schema",
-                      body: {
-                        content: responses[code].content,
-                      },
-                    }),
-                  }),
-                ],
-              });
-            }),
-          }),
-        ],
-      }),
-    ],
-  });
-}
+// export function createStatusCodes({ label, id, responses }: Props) {
+//   if (responses === undefined) {
+//     return undefined;
+//   }
+
+//   const codes = Object.keys(responses);
+//   if (codes.length === 0) {
+//     return undefined;
+//   }
+
+//   return create("div", {
+//     children: [
+//       create("div", {
+//         children: [
+//           create("ApiTabs", {
+//             label,
+//             id,
+//             children: codes.map((code) => {
+//               const responseHeaders: any = responses[code].headers;
+//               return create("TabItem", {
+//                 label: code,
+//                 value: code,
+//                 children: [
+//                   create("div", {
+//                     children: createDescription(responses[code].description),
+//                   }),
+//                   responseHeaders &&
+//                     createDetails({
+//                       className: "openapi-markdown__details",
+//                       "data-collapsed": true,
+//                       open: false,
+//                       style: { textAlign: "left", marginBottom: "1rem" },
+//                       children: [
+//                         createDetailsSummary({
+//                           children: [
+//                             create("strong", {
+//                               children: "Response Headers",
+//                             }),
+//                           ],
+//                         }),
+//                         createResponseHeaders(responseHeaders),
+//                       ],
+//                     }),
+//                   create("div", {
+//                     children: createResponseSchema({
+//                       title: "Schema",
+//                       body: {
+//                         content: responses[code].content,
+//                       },
+//                     }),
+//                   }),
+//                 ],
+//               });
+//             }),
+//           }),
+//         ],
+//       }),
+//     ],
+//   });
+// }
