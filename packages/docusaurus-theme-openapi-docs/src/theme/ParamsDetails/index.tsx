8ca203948ada02e6aae4ac72238d834be0683e80
Steven Serrata <sserrata@paloaltonetworks.com>
2025-01-14 16:43:31 -0600

    remove unused import

--- packages/docusaurus-theme-openapi-docs/src/theme/ParamsDetails/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ParamsDetails/index.tsx
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  * ========================================================================== */
 
-import React, { Suspense } from "react";
+import React from "react";
 
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import Details from "@theme/Details";
