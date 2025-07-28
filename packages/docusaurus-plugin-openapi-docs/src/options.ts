afa156ad1fee35e32d231fdccf550cb28ab7e49f
Devon <86693904+Devon-White@users.noreply.github.com>
2025-07-28 10:30:34 -0400

    Add maskCredentials as a pluginOption (#1212)
    * Add maskCredentials as a pluginOption
    
    * fix formatting to be consistent

--- packages/docusaurus-plugin-openapi-docs/src/options.ts
+++ packages/docusaurus-plugin-openapi-docs/src/options.ts
@@ -49,6 +49,7 @@ export const OptionsSchema = Joi.object({
         markdownGenerators: markdownGenerators,
         showSchemas: Joi.boolean(),
         disableCompression: Joi.boolean(),
+        maskCredentials: Joi.boolean(),
         version: Joi.string().when("versions", {
           is: Joi.exist(),
           then: Joi.required(),
