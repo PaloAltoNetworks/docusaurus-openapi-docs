fbca820e16a1b00831e0ab312ec7e7c3a12e7604
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2025-08-25 10:58:33 -0400

    refactor: use docusaurus sidebar types

--- packages/docusaurus-theme-openapi-docs/src/theme-openapi.d.ts
+++ packages/docusaurus-theme-openapi-docs/src/theme-openapi.d.ts
@@ -7,43 +7,15 @@
 
 /// <reference types="docusaurus-plugin-openapi-docs" />
 
+export type {
+  PropSidebarItemCategory,
+  SidebarItemLink,
+  PropSidebar,
+  PropSidebarItem,
+  PropSidebars,
+} from "@docusaurus/plugin-content-docs/lib/sidebars/types";
+
 /* eslint-disable @typescript-eslint/no-use-before-define */
-
-declare module "@docusaurus/plugin-content-docs-types" {
-  // Makes all properties visible when hovering over the type
-  type Expand<T extends Record<string, unknown>> = { [P in keyof T]: T[P] };
-
-  export type SidebarItemBase = {
-    className?: string;
-    customProps?: Record<string, unknown>;
-  };
-
-  export type SidebarItemLink = SidebarItemBase & {
-    type: "link";
-    href: string;
-    label: string;
-    docId: string;
-  };
-
-  type SidebarItemCategoryBase = SidebarItemBase & {
-    type: "category";
-    label: string;
-    collapsed: boolean;
-    collapsible: boolean;
-  };
-
-  export type PropSidebarItemCategory = Expand<
-    SidebarItemCategoryBase & {
-      items: PropSidebarItem[];
-    }
-  >;
-
-  export type PropSidebarItem = SidebarItemLink | PropSidebarItemCategory;
-  export type PropSidebar = PropSidebarItem[];
-  export type PropSidebars = {
-    [sidebarId: string]: PropSidebar;
-  };
-}
 
 declare module "docusaurus-theme-openapi-docs" {
   export type ThemeConfig = Partial<import("./types").ThemeConfig>;
